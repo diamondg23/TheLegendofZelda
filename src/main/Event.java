589a1b7dea997da1a53a969ca03ea0cbb5226eb6
diamondg23 <diamondgirly23@outlook.com>
2025-11-10 18:18:00 -0700

    Added a event class to eventually create a event system to handle everything that happens in the game

---
+++ src/main/Event.java
@@ -0,0 +1,23 @@
+package main;
+
+public class Event {
+	public enum events {
+		PLAYERMOVEMENT(),
+		ENEMYMOVEMENT(),
+		//this can be like a arrow being shot or something else moving
+		MISCMOVEMENT(),
+		BOMBPLACED(),
+		
+	}
+	events event;
+	int eventID;
+	public Event(events event) {
+		this.event = event;
+		eventID = event.ordinal();
+	}
+	public Event(int eventID) {
+		this.event = events.values()[eventID];
+		this.eventID = eventID;
+		
+	}
+}
